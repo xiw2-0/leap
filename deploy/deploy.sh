3c8dac3b475269cf9d2af225bc28a5571c485cf1
Wind <xiw2.0@outlook.com>
2026-03-28 23:24:04 +0800

    Update deploy scripts to make the deployment easier.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,10 +2,11 @@ export WINEPREFIX=/opt/leap/wine
 
 sudo -u leap mkdir -p /opt/leap/release/$(date +%Y%m%d)
 
-sudo -u leap mv /home/x/*.whl /opt/leap/release/$(date +%Y%m%d)
-sudo -u leap mv /home/x/.env /opt/leap/release/$(date +%Y%m%d)
+sudo -u leap cp /tmp/leap/*.whl /opt/leap/release/$(date +%Y%m%d)
+sudo -u leap cp /tmp/leap/.env /opt/leap/release/$(date +%Y%m%d)
 
-sudo -u leap ln -sf /opt/leap/release/$(date +%Y%m%d) /opt/leap/current
+sudo -u leap rm /opt/leap/current
+sudo -u leap ln -s /opt/leap/release/$(date +%Y%m%d) /opt/leap/current
 
 sudo -E -u leap wine python -m pip uninstall -y leap
 sudo -E -u leap wine python -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple Z:/opt/leap/current/leap-0.1.0-py3-none-any.whl
--- deploy/package.ps1
+++ deploy/package.ps1
@@ -1,5 +1,6 @@
 poetry build
 
-scp dist/*.whl x@139.196.19.116:/home/x/
-scp .env x@139.196.19.116:/home/x/
-scp ./deploy/deploy.sh x@139.196.19.116:/home/x/
+ssh x@139.196.19.116 'mkdir -p /tmp/leap'
+scp dist/*.whl x@139.196.19.116:/tmp/leap/
+scp .env x@139.196.19.116:/tmp/leap/
+scp ./deploy/deploy.sh x@139.196.19.116:/tmp/leap/
